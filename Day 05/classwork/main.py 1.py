58eb85b8107196220ca62adcba44225632ccd109
niko826 <nikolozlicheli@gmail.com>
2025-03-04 23:21:44 +0400

    commit

---
+++ Day 05/classwork/main.py 1.py
@@ -0,0 +1,5 @@
+age = int(input("enter your age: "))  # შევეკითხე ასაკი 
+if  age >= 18:
+    print("you can enter the club") # თუ 18 წლის არ არის კლუბში ვერ შევა და თუ 18 წ მეტის არის შევა 
+else:
+    print("entrey forbidden")
