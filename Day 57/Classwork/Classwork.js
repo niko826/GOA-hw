67b1c1057098044fbf7da4f8443e6151a792a55c
niko826 <nikolozlicheli@gmail.com>
2025-07-09 00:37:08 +0400

    Add files via upload

---
+++ Day 57/Classwork/Classwork.js
@@ -0,0 +1,16 @@
+function CARS(car) {
+  if (car.toLowerCase() === "მერსედესი") {
+    console.log("კარგი არჩევანია");
+  } else if (car.toLowerCase() === "ტესლა") {
+    console.log("ცუდი არჩევანია");
+  } else if (car.toLowerCase() === "ოპელი") {
+    console.log("საშუალო არჩევანია");
+  } else {
+    console.log("ვერ გეტყვი");
+  }
+}
+
+
+
+
+
---
+++ Day 57/Classwork/Classwork1.js
@@ -0,0 +1,10 @@
+const favoriteFilm = "Avengers";
+
+const filmList = ["Avengers", "Inception", "The Matrix", "Avatar"];
+
+// შემოწმება ფილმი არის თუ არა სიაში
+if (filmList.includes(favoriteFilm)) {
+  console.log("It is a good film");
+} else {
+  console.log("The Film is not good");
+}
