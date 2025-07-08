feded5ab72c9d20b4ce8ee80de8a9d5a09a3d99b
niko826 <nikolozlicheli@gmail.com>
2025-07-09 00:36:44 +0400

    Add files via upload

---
+++ Day 55/Classwork/Classwork1.js
@@ -0,0 +1,9 @@
+let maxNumber = 100;
+let randomNumber = Math.round(Math.random() * maxNumber);
+console.log(" random რიცხვი არის:", randomNumber);
+
+let name = "Nika";
+console.log(`Hello My name is "${name}"`);
+
+let fullName = "Nikoloz licheli";
+console.log(`სახელი და გვარის სიგრძეა: ${fullName.length}`);
