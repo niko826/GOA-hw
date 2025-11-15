dbc2edf776987e717d9b8b66f50a2fb51d8e1f62
niko826 <nikolozlicheli@gmail.com>
2025-11-15 13:16:44 +0400

    Delete Day111 directory

--- Day111/Classwork/Classwork/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class ErrorBoundary extends Component {
-  render() {
-    return <h1>ErrorBoundary Component</h1>;
-  }
-}
-
-export default ErrorBoundary;
