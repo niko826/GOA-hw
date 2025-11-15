96a21dbd6cd500eaa8996a85c897f0174e79ec46
niko826 <nikolozlicheli@gmail.com>
2025-11-15 13:13:02 +0400

    Add files via upload

---
+++ Day 102/Homework/Homework/link.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="flex gap-4 bg-gray-800 text-white p-4">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
