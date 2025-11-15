df5d3998243bd5dfa4476166bf2ff710e8cd64c9
niko826 <nikolozlicheli@gmail.com>
2025-11-15 13:27:52 +0400

    Add files via upload

---
+++ Day111/Classwork/Classwork/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  render() {
+    return <h1>ErrorBoundary Component</h1>;
+  }
+}
+
+export default ErrorBoundary;
---
+++ Day111/Homework/App.jsx
@@ -0,0 +1,9 @@
+class ErrorBoundary extends Component {
+  // render მეთოდი—აქ წერ როგორ უნდა გამოისახოს კომპონენტი
+  render() {
+    // უბრალოდ გამოაქვს ტექსტი ekranz
+    return <h1>ErrorBoundary Component</h1>;
+  }
+}
+
+export default ErrorBoundary; // კომპონენტის გატანა სხვაგან გამოსაყენებლად
