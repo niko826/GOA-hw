92f34bf865ab50030a07e76be3ff0d6d1c70ae36
niko826 <nikolozlicheli@gmail.com>
2025-11-15 13:23:05 +0400

    Add files via upload

---
+++ Day101/Homework/Homework/tailwind.js
@@ -0,0 +1,11 @@
+content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+theme: {
+  extend: {
+    colors: {
+      primary: "#2563eb",
+      secondary: "#1e293b",
+      accent: "#f59e0b",
+    },
+  },
+},
+plugins: [],
